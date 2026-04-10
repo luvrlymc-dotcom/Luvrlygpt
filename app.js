23d58a65b3b9f815f57b240221efaafe9947d154
luvrlymc-dotcom <luvrlymc@gmail.com>
2026-04-10 11:13:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const HOST = "127.0.0.1";
 
 // 🔗 Gist RAW
 const GIST_RAW_URL =
-  "https://gist.githubusercontent.com/luvrlymc-dotcom/6e1411dd6056806ae7611319eee94de7/raw/47677bd3d6a85af93339987a35713229138edc67/gistfile1.txt";
+  "https://gist.githubusercontent.com/luvrlymc-dotcom/6e1411dd6056806ae7611319eee94de7/raw/8c8b8d9210524f6a6191a4b778b93c392c4c6007/gistfile1.txt";
 
 // ================= CACHE =================
 let cachedHTML = "<h1>Loading...</h1>";
