62f7d861efe583b926460def3eaf4ac01c107296
luvrlymc-dotcom <luvrlymc@gmail.com>
2026-06-26 13:34:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
-// ====================== CORS (RẤT QUAN TRỌNG) ======================
+// ====================== CORS (RẤT QUAN TRỌNG) =====================
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
