d058439523d7d25d34b67fa10734ddec616273e3
luvrlymc-dotcom <luvrlymc@gmail.com>
2026-07-27 22:39:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(fileUpload({
 }));
 
 // Gist RAW.
-const GIST_RAW_URL = "https://gist.githubusercontent.com/luvrlymc-dotcom/36867f334060df8b45b0e8a74b66ce16/raw/3a159210a3af02b2e08d0a29dd70c6e7d17843e3/gistfile1.txt";
+const GIST_RAW_URL = "https://gist.githubusercontent.com/luvrlymc-dotcom/36867f334060df8b45b0e8a74b66ce16/raw/fceeab52f5905b7a62feca131aba846c7edc3c46/gistfile1.txt";
 
 // Cache
 let cachedHTML = "<h1>Server is starting...</h1>";
